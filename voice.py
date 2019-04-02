351fd49650074f67ccb79215a9dda97852b0daf3
Shawn Polson <sapols@yahoo.com>
2019-04-02 17:14:35 -0400

    Upload a python script for voice -> OSC commands

---
+++ voice.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+from pythonosc import udp_client
+
+r = sr.Recognizer()
+client = udp_client.SimpleUDPClient("localhost", 12000)
+
+while(True):
+    with sr.Microphone() as source:
+        print('Speak Anything: ')
+        audio = r.listen(source)
+
+        try:
+            text = r.recognize_google(audio)
+            if (text == 'elevator go up'):
+            	client.send_message("/up_voice", 1 )
+            elif (text == 'elevator go down'):
+            	client.send_message("/down_voice", 2 )
+            
+            print('You said:  {}'.format(text))
+        except Exception as e:
+            print(e)
+            print('Sorry, could not recognize your voice!')
+            
